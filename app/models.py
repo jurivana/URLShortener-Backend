64dfa823252fddb4b074656d1d696ffeeb4eda6c
Juri Vana <jvana@uos.de>
2022-02-14 18:45:51 +0100

    First version

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Mapping(models.Model):
+    path = models.CharField(primary_key=True, max_length=32, editable=False)
+    url = models.URLField(max_length=512)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from uuid import uuid4
 
-# Create your views here.
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Mapping
+
+
+class Redirect(APIView):
+    def get(self, request, path):
+        return redirect(get_object_or_404(Mapping, path=path).url)
+
+
+class Generate(APIView):
+    def post(self, request):
+        data = request.data
+        path = data['path'] if 'path' in data else str(uuid4())[:7]
+        url = data['url']
+        try:
+            Mapping.objects.create(path=path, url=url)
+        except Exception:
+            return Response(status=409)
+        return JsonResponse({'url': f'localhost:8000/redirect/{path}'}, status=201)
